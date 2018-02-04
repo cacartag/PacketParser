0c542626127d502de6f78912958499f9293cccfa
cacartag <cacartag@email.uark.edu>
2018-02-04 01:46:15 -0600

    added Ethernetparser class

---
+++ EthernetParser.java
@@ -0,0 +1,74 @@
+import java.nio.ByteBuffer;
+
+public class EthernetParser{
+    private byte [] destBytes;
+    private String destString;    
+    
+    private byte [] srcBytes;
+    private String srcString;
+
+    private byte [] typeBytes;
+    private String typeString;
+    
+    public byte [] getDestinationBytes(){ return destBytes; }
+    public String getDestinationString(){ return destString; };
+    public void setDestination(byte [] packet)
+    {
+        for(int index = 0; index < 6; index++)
+        {
+            destBytes[index] = packet[index];            
+        }
+        
+        for(byte current : destBytes)
+        {
+            destString += String.format("%02X",current);
+        }         
+    }
+    
+    
+    public byte [] getSourceBytes(){ return srcBytes; }
+    public String getSourceString(){ return srcString; };
+    public void setSource(byte [] packet)
+    {
+        for(int index = 0; index < 6; index++)
+        {
+            srcBytes[index] = packet[index + 6]; 
+        }
+
+        for(byte current : srcBytes)
+        {
+            srcString += String.format("%02X",current);
+        }        
+    }
+
+    public byte [] getTypeBytes(){ return typeBytes; }
+    public String getTypeString(){ return typeString; }
+    public void setType(byte [] packet)
+    {
+        for(int index = 0; index < 2; index++)
+        {
+            typeBytes[index] = packet[index + 12];
+        }
+        
+        for(byte current : typeBytes)
+        {
+            typeString += String.format("%02X",current);
+        }
+    }
+    
+    
+    EthernetParser()
+    {
+        destBytes = new byte[6];
+        destString = "";
+        
+        srcBytes = new byte[6];
+        srcString = "";
+        
+        typeBytes = new byte[2];
+        typeString = "";
+    }
+    
+}
+
+
---
+++ Main.java
@@ -0,0 +1,44 @@
+import java.nio.ByteBuffer;
+import java.util.Scanner;
+
+public class Main{
+    
+    public static void main(String [] args) throws Exception
+    {
+        SimplePacketDriver driver = new SimplePacketDriver();
+        String[] adapters = driver.getAdapterNames();
+        Scanner sc = new Scanner(System.in);
+        EthernetParser eth = new EthernetParser();
+        
+        System.out.println("Adapter found are:");
+        for (int index = 0; index < adapters.length; index++)
+        {
+            System.out.println("("+index+"): "+adapters[index]);
+        }
+        
+        System.out.println("Which adapter do you choose?");
+        
+        int adapterIndex = sc.nextInt();
+        
+        if(driver.openAdapter(adapters[adapterIndex]))
+        {
+            System.out.println("adapter "+ adapters[adapterIndex] + " open");
+        }
+        
+        byte [] packet = driver.readPacket();
+        
+        ByteBuffer Packet = ByteBuffer.wrap(packet);
+        
+        System.out.println("Packet: "+Packet+" with capacity: "+Packet.capacity());
+        System.out.println(driver.byteArrayToString(packet));
+        
+        eth.setDestination(packet);
+        eth.setSource(packet);
+        eth.setType(packet);    
+
+        System.out.println("Destination: " + eth.getDestinationString());
+        System.out.println("Source: " + eth.getSourceString());
+        System.out.println("Type: " + eth.getTypeString());
+    }
+    
+}
