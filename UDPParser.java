03d89a47356f9ead0d54554d81aa0c0f50b72580
cacartag <cacartag@email.uark.edu>
2018-02-21 09:09:50 -0600

    Wrote udp parser needs to be tested

--- UDPParser.java
+++ UDPParser.java
@@ -12,8 +12,39 @@ public class UDPParser
     private int checkSum;
     private String checkSumString;
     
+    public String getSourcePortString(){ return sourcePortString; }
+    
+    public String getDestinationPortString() { return destinationPortString; }
+    
+    public String getLengthString() { return lengthString; }
+    
+    public String getCheckSumString() { return checkSumString; }
+    
     public void parsePacket(byte [] packet)
     {
+        sourcePort = (int)(packet[34] & 0xFF);
+        sourcePort = sourcePort << 8;
+        sourcePort = sourcePort + (int)(packet[35] & 0xFF);
+        
+        sourcePortString = Integer.toString(sourcePort);
+        
+        destinationPort = (int)(packet[36] & 0xFF);
+        destinationPort = destinationPort << 8;
+        destinationPort = destinationPort + (int)(packet[37] & 0xFF);
+        
+        destinationPortString = Integer.toString(destinationPort);
+        
+        length = (int)(packet[38] & 0xFF);
+        length = length << 8;
+        length = length + (int)(packet[39] & 0xFF);
+        
+        lengthString = Integer.toString(length);
+        
+        checkSum = (int)(packet[40] & 0xFF);
+        checkSum = checkSum << 8;
+        checkSum = checkSum + (int)(packet[41] & 0xFF);
+        
+        checkSumString = Integer.toString(checkSum);
         
     }
     
